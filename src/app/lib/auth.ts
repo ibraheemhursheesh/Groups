92f327881fcdecc96a748ec963babcbc3d4b61de
ibrahimharchiche <ibrahimharchiche44@gmail.com>
2026-07-28 11:37:11 +0100

    set up

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,13 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+
+export const auth = betterAuth({
+  database: new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }, // required for Supabase
+  }),
+  //...other options
+  emailAndPassword: {
+    enabled: true,
+  },
+});
